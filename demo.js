dcb278fafba33dfd86564ad879928d6846bfdef0
User <user@example.com>
2025-03-15 16:13:09 +0300

    completed

--- demo.js
+++ demo.js
@@ -10,53 +10,38 @@ const colorStates = {
   "": "red"
 };
 
-// STEP 1: The line of code below is what lets JavaScript find the elements that
-// we want to make clickable. Without JavaScript, clicking on these heart shapes
-// does nothing. Uncomment the code and refresh the demo page. 
+document.addEventListener("DOMContentLoaded", () => {
+  // STEP 1: Select all heart icons
+  const articleHearts = document.querySelectorAll(".like-glyph");
 
-// const articleHearts = document.querySelectorAll(".like-glyph");
+  function likeCallback(e) {
+    const heart = e.target;
+    mimicServerCall()
+      .then(function(serverMessage){
+        alert("You notified the server!");
+        heart.innerText = glyphStates[heart.innerText];
+        heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(function(error) {
+        alert("Something went wrong!");
+      });
+  }
 
-function likeCallback(e) {
-  const heart = e.target;
-  mimicServerCall()
-    .then(function(serverMessage){
-      // STEP 2: Uncomment the 3 lines after the alert.
-      // Here we're using Pillar 1 (DOM Manipulation) to update the screen and
-      // mimicking Pillar 3 (Server Communication) to only update the screen if
-      // the sending of information to the server succeeds.
-      alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
-    })
-    .catch(function(error) {
-      alert("Something went wrong!");
-    });
-}
+  // STEP 3: Add event listeners to each heart icon
+  articleHearts.forEach((heart) => {
+    heart.addEventListener("click", likeCallback);
+  });
+});
 
-// STEP 3: In order for the call to the server and the update of the screen to
-// work, we need to add a click event listener to the elements we identified in
-// STEP 1. That's Pillar 2, event handling. Uncomment this code:
-
-// for (const glyph of articleHearts) {
-//   glyph.addEventListener("click", likeCallback);
-// }
-
-// STEP 4: 
-
-// When all the STEPs' code changes have been complete, you'll be able to see a
-// working demonstration of our reference example. Sure, it's maybe not as
-// pretty as a professional site, but they're only different from our site in
-// style, not substance.
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
-
-function mimicServerCall() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve("Pretend remote server notified of action!");
+// Mock server call function
+function mimicServerCall(url = "", config = {}) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.2) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
     }, 300);
   });
 }
